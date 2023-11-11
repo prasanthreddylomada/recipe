dc4b1a6232d641a635d12b0ca620fd4601386557
prasanthreddylomada <lomadaprasanthreddy@gmail.com>
2023-11-12 00:25:35 +0530

    added popups for signup and login

--- src/HomePage.js
+++ src/HomePage.js
@@ -2,6 +2,79 @@ import React, { Component } from 'react';
 import utensilsIcon from './images/Icon utensils.png';
 import cooking from './images/cooking.png'
 class HomePage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          signupPopup: false,
+          browseRecipesPopup: false,
+          signupUsername: '',
+          signupPassword: '',
+          signupConfirmPassword: '',
+          loginUsername: '',
+          loginPassword: '',
+        };
+      }
+    
+      handleOpenSignupPopup = () => {
+        this.setState({ signupPopup: true });
+      };
+    
+      handleCloseSignupPopup = () => {
+        this.setState({ signupPopup: false });
+      };
+    
+      handleOpenBrowseRecipesPopup = () => {
+        this.setState({ browseRecipesPopup: true });
+      };
+    
+      handleCloseBrowseRecipesPopup = () => {
+        this.setState({ browseRecipesPopup: false });
+      };
+    
+      handleCreateAccount = () => {
+        if (this.state.signupPassword !== this.state.signupConfirmPassword) {
+          alert('Password and Confirm Password do not match!');
+          return;
+        }
+    
+        console.log('Username:', this.state.signupUsername);
+        console.log('Password:', this.state.signupPassword);
+    
+        // Close the signup popup after handling the account creation
+        this.handleCloseSignupPopup();
+      };
+    
+      handleLogin = () => {
+        console.log('Login Username:', this.state.loginUsername);
+        console.log('Login Password:', this.state.loginPassword);
+    
+        // Handle login logic here
+    
+        // Close the login popup after handling the login
+        this.handleCloseBrowseRecipesPopup();
+      };
+    
+      handleSignupUsernameChange = (event) => {
+        this.setState({ signupUsername: event.target.value });
+      };
+    
+      handleSignupPasswordChange = (event) => {
+        this.setState({ signupPassword: event.target.value });
+      };
+    
+      handleSignupConfirmPasswordChange = (event) => {
+        this.setState({ signupConfirmPassword: event.target.value });
+      };
+    
+      handleLoginUsernameChange = (event) => {
+        this.setState({ loginUsername: event.target.value });
+      };
+    
+      handleLoginPasswordChange = (event) => {
+        this.setState({ loginPassword: event.target.value });
+      };
+    
   render() {
     const { theme } = this.props;
 
@@ -31,7 +104,16 @@ class HomePage extends Component {
         border: 'none',
         borderRadius: '5px',
         cursor: 'pointer',
-        
+      },
+      signupbutton: {
+        backgroundColor: theme.colors.button,
+        color: theme.colors.white,
+        padding: '10px 20px',
+        border: 'none',
+        borderRadius: '5px',
+        cursor: 'pointer',
+        margin:'10px 3px 2px 4px',
+        fontSize:'15px'
       },
       bottomcontainer: {
         display: 'flex',
@@ -47,8 +129,7 @@ class HomePage extends Component {
       },
       bottombuttoncontainer:{
         display:'flex',
-        justifyContent:'space-evenly',
-        marginTop:'2%'
+        marginTop:'5%'
       }
     };
 
@@ -65,8 +146,45 @@ class HomePage extends Component {
             />
             </div>
             <div style={styles.buttonContainer}>
-            <button style={styles.button}>Join Now</button>
+                <button onClick={this.handleOpenSignupPopup} style={styles.button}>Join Now</button>
             </div>
+            {/* Signup Popup */}
+            {this.state.signupPopup && (
+                <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', background: 'rgba(0,0,0,0.5)' }}>
+                <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', background: 'white', padding: '40px', borderRadius: '5px' }}>
+                    <div style={{ fontSize: '40px', fontWeight: 'bold', marginBottom: '10px' }}>Create Account</div>
+                    <div style={{ display: 'flex', flexDirection: 'column' }}>
+                    <input style={{ marginTop: '5px', marginBottom: '5px', fontSize: '20px' }} type="text" placeholder="Username" value={this.state.signupUsername} onChange={this.handleSignupUsernameChange} />
+                    <input style={{ marginTop: '5px', marginBottom: '5px', fontSize: '20px' }} type="password" placeholder="Password" value={this.state.signupPassword} onChange={this.handleSignupPasswordChange} />
+                    <input style={{ marginTop: '5px', marginBottom: '5px', fontSize: '20px' }} type="password" placeholder="Confirm Password" value={this.state.signupConfirmPassword} onChange={this.handleSignupConfirmPasswordChange} />
+                    <div>
+                        <button style={styles.signupbutton} onClick={this.handleCreateAccount}>Create Account</button>
+                        <button style={styles.signupbutton} onClick={this.handleCloseSignupPopup}>Cancel</button>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            )}
+            {/* Login Popup */}
+            {this.state.browseRecipesPopup && (
+                <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', background: 'rgba(0,0,0,0.5)' }}>
+                <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', background: 'white', padding: '40px', borderRadius: '5px' }}>
+                    <div style={{ fontSize: '40px', fontWeight: 'bold', marginBottom: '10px' }}>Enter Credentials</div>
+                    <div style={{ display: 'flex', flexDirection: 'column' }}>
+                    <input style={{ marginTop: '5px', marginBottom: '5px', fontSize: '20px' }} type="text" placeholder="Username" value={this.state.loginUsername} onChange={this.handleLoginUsernameChange} />
+                    <input style={{ marginTop: '5px', marginBottom: '5px', fontSize: '20px' }} type="password" placeholder="Password" value={this.state.loginPassword} onChange={this.handleLoginPasswordChange} />
+                    <div>
+                        <button style={styles.signupbutton} onClick={this.handleLogin}>
+                        Submit
+                        </button>
+                        <button style={styles.signupbutton} onClick={this.handleCloseBrowseRecipesPopup}>
+                        Cancel
+                        </button>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            )}
         </div>
         <div style={styles.bottomcontainer}>
             <div style={{ ...styles.column, ...styles.column1 }}>
@@ -77,8 +195,7 @@ class HomePage extends Component {
                     Introducing the Ultimate Recipe App:Explore,Find,Cook and Share
                 </div>
                 <div style={styles.bottombuttoncontainer}>
-                    <button style={{...styles.button,fontSize:'20px',paddingInline:'7%'}}>Get Started</button>
-                    <button style={{...styles.button,fontSize:'20px'}}>Find Recipes</button>
+                    <button onClick={this.handleOpenBrowseRecipesPopup} style={{...styles.button,fontSize:'20px',paddingInline:'5%'}}>Browse Recipes</button>
                 </div>
             </div>
             <div style={{ ...styles.column}}>
